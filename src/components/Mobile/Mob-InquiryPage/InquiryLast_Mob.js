c93f5f2038601b54b19a480d22b53832b5322dfa
shyoon840 <shyoon840@handong.ac.kr>
2023-05-05 18:51:51 +0900

    update InquiryLast_mob.js

--- src/components/Mobile/Mob-InquiryPage/InquiryLast_Mob.js
+++ src/components/Mobile/Mob-InquiryPage/InquiryLast_Mob.js
@@ -1,30 +1,84 @@
 import styled, { ThemeProvider } from 'styled-components';
 import { theme } from '../../../styles/theme';
+import React from 'react';
 
 const Header7 = styled.div`
-  font-size: ${props => props.theme.Mob_fontSizes.Header7};
-  font-weight: ${props => props.theme.fontWeights.Header7};
-  color: #FFFFFF;
-  font-family: 'NanumSquare Neo';
-  margin-bottom: 3.7500vw;
-  white-space: pre-line;
-`;
+    font-size: ${props => props.theme.Mob_fontSizes.Header8};
+    font-weight: ${props => props.theme.fontWeights.Header8};
+    color: #FFFFFF;
+    font-family: 'NanumSquare Neo';
+    line-height: 1.45;
+    margin-bottom: 5.500vw;
+    white-space: pre-line;
+    text-align : center;
+  `;
 
 const PartDiv = styled.div`
-    padding-left:16px;
-    padding-top: 45px;
-    height: 2445px;
+    padding-left:12.0556vw;
+    padding-right: 112px;
+    padding-top: 100px;
+    padding-bottom: 65vw; // 원하는 만큼 값을 조절
+    height: 62vw;
+    position: relative;
 `;
 
-function InquiryLastMob() {
+const Subtitle3 = styled.div`
+    font-size: ${props => props.theme.Mob_fontSizes.Body1};
+    font-weight: ${props => props.theme.fontWeights.Body1};
+    color : #FFFFFF;
+    font-family: 'NanumSquare Neo';
+    margin-bottom: 3.7500vw;
+    white-space: pre-line;
+    text-align: center;
+`;
+
+const Frame15 = styled.button`
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    padding: 1.0417vw 8.3333vw;
+    gap: 0.6944vw;
+    margin-top: -1.3vw;
+    /* position: absolute; */
+    width: 32.2667vw;
+    height: 13.8667vw;
+    /* left: calc(50% - 23.0556vw/2); */
+    /* left: 8.8194vw; */
+    margin-left: 20vw;
+    margin-top: 13.8667vw;
+    border:none;
+    background: #5262F5;
+    border-radius: 10px;
+`
+
+const Header5 = styled.div`
+    font-size: ${props => props.theme.Mob_fontSizes.ButtonText1};
+    font-weight: ${props => props.theme.fontWeights.ButtonText1};
+    color : #FFFFFF;
+    font-family: 'NanumSquare Neo';
+    margin-bottom: -0.6600vw;
+    white-space: pre-line;
+    text-align: center;
+    white-space: nowrap;
+`
+
+function InquiryLast() {
+    const handleClick = () => {
+        window.location.href = "https://pard.channel.io/lounge";
+    };
 
     return (
         <PartDiv>
-             <ThemeProvider theme={theme}>
-            <Header7>문의 마지막 페이지 모바일</Header7>
+            <ThemeProvider theme={theme}>
+                <Header7>더 궁금하신<br></br>내용이 있거나<br></br>문의사항이<br></br>있으신가요?</Header7>
+                <Subtitle3>동아리 관련, 프로젝트, 후원 등<br></br>언제든지 문의해 주세요.</Subtitle3>
+                <Frame15 onClick={handleClick}>
+                    <Header5>문의하기</Header5>
+                </Frame15>
             </ThemeProvider>
         </PartDiv>
     );
 }
 
-export default InquiryLastMob;
+export default InquiryLast;
--- src/components/Web/Web-InquiryPage/InquriyLast.js
+++ src/components/Web/Web-InquiryPage/InquriyLast.js
@@ -11,74 +11,73 @@ const Header7 = styled.div`
   margin-bottom: 2.500vw;
   white-space: pre-line;
   text-align : center;
-
   `;
 
 const PartDiv = styled.div`
-    padding-left:5.5556vw;
-    padding-right: 112px;
-    padding-top: 100px;
-    height: 62vw;
-    position: relative;
+  padding-left:5.5556vw;
+  padding-right: 112px;
+  padding-top: 100px;
+  height: 62vw;
+  position: relative;
 `;
 
 const Subtitle3 = styled.div`
-    font-size: ${props => props.theme.Web_fontSizes.Subtitle3};
-    font-weight: ${props => props.theme.fontWeights.Subtitle3};
-    color : #FFFFFF;
-    font-family: 'NanumSquare Neo';
-    margin-bottom: 3.7500vw;
-    white-space: pre-line;
-    text-align: center;
+  font-size: ${props => props.theme.Web_fontSizes.Subtitle3};
+  font-weight: ${props => props.theme.fontWeights.Subtitle3};
+  color : #FFFFFF;
+  font-family: 'NanumSquare Neo';
+  margin-bottom: 3.7500vw;
+  white-space: pre-line;
+  text-align: center;
 `;
 
 const Frame15 = styled.button`
-    display: flex;
-    flex-direction: row;
-    justify-content: center;
-    align-items: center;
-    padding: 1.0417vw 8.3333vw;
-    gap: 0.6944vw;
-    margin-top: -1.3vw;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  padding: 1.0417vw 8.3333vw;
+  gap: 0.6944vw;
+  margin-top: -1.3vw;
 
-    position: absolute;
-    width: 23.0556vw;
-    height: 4.4444vw;
-    /* left: calc(50% - 23.0556vw/2); */
-    left: 33.183vw;
-    top: 34.9306vw;
-    border:none;
-    background: #5262F5;
-    border-radius: 0.6944vw;
+  position: absolute;
+  width: 23.0556vw;
+  height: 4.4444vw;
+  /* left: calc(50% - 23.0556vw/2); */
+  margin-left: 31.0722vw;
+  margin-top: 7.9306vw;
+  border:none;
+  background: #5262F5;
+  border-radius: 0.6944vw;
 `
 
 const Header5 = styled.div`
-    font-size: ${props => props.theme.Web_fontSizes.Header5};
-    font-weight: ${props => props.theme.fontWeights.Header5};
-    color : #FFFFFF;
-    font-family: 'NanumSquare Neo';
-    margin-bottom: 0.2600vw;
-    white-space: pre-line;
-    text-align: center;
-    white-space: nowrap;
+  font-size: ${props => props.theme.Web_fontSizes.Header5};
+  font-weight: ${props => props.theme.fontWeights.Header5};
+  color : #FFFFFF;
+  font-family: 'NanumSquare Neo';
+  margin-bottom: 0.2600vw;
+  white-space: pre-line;
+  text-align: center;
+  white-space: nowrap;
 `
 
 function InquiryLast() {
-        const handleClick = () => {
-          window.location.href = "https://pard.channel.io/lounge";
-        };
+  const handleClick = () => {
+    window.location.href = "https://pard.channel.io/lounge";
+  };
 
-    return (
-        <PartDiv>
-            <ThemeProvider theme={theme}>
-                <Header7>더 궁금하신 내용이 있거나<br></br>문의사항이 있으신가요?</Header7>
-                <Subtitle3>동아리 관련, 프로젝트, 후원 등 언제든지 문의해 주세요.</Subtitle3>
-                <Frame15 onClick={handleClick}>
-                    <Header5>문의하기</Header5>
-                </Frame15>
-            </ThemeProvider>
-        </PartDiv>
-    );
+  return (
+    <PartDiv>
+      <ThemeProvider theme={theme}>
+        <Header7>더 궁금하신 내용이 있거나<br></br>문의사항이 있으신가요?</Header7>
+        <Subtitle3>동아리 관련, 프로젝트, 후원 등 언제든지 문의해 주세요.</Subtitle3>
+        <Frame15 onClick={handleClick}>
+          <Header5>문의하기</Header5>
+        </Frame15>
+      </ThemeProvider>
+    </PartDiv>
+  );
 }
 
 export default InquiryLast;
