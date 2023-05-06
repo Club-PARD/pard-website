f0ed432c8ce5225c861461fcd4c1beea57370bb6
dnqudgml12 <21800454@handomg.ac.kr>
2023-05-06 11:11:26 +0900

    scroll 수정

--- src/components/Mobile/Mob-InquiryPage/InquiryContact_Mob.js
+++ src/components/Mobile/Mob-InquiryPage/InquiryContact_Mob.js
@@ -14,7 +14,7 @@ const Header7 = styled.div`
 const PartDiv = styled.div`
     padding-left:16px;
     padding-top: 45px;
-    height: 2445px;
+    height: 1000px;
 `;
 //<Header7>문의하세요! 모바일</Header7>
 function InquiryContactMob() {
