4ad0f57d980d810a01a6ec8b94dcaf0465a268a2
dasdfadssda <quddkflty@gmail.com>
2023-05-07 21:50:09 +0900

    About Page Setting

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -4,7 +4,12 @@ import Navbar from "../components/NavBar";
 import styled from "styled-components";
 import { useMediaQuery } from 'react-responsive'
 import NavBarMov from "../components/NavBarMov";
-
+import AboutVision from "../components/Web/Web-AboutPage/Web-AboutVision"
+import AboutFirst from "../components/Web/Web-AboutPage/Web-AboutFirst";
+import AboutPart from "../components/Web/Web-AboutPage/Web-AboutPart";
+import AboutProgram from "../components/Web/Web-AboutPage/Web-AboutProgram";
+import AboutMentor from "../components/Web/Web-AboutPage/Web-AboutMentor";
+import AboutLast from "../components/Web/Web-AboutPage/Web-AboutLast";
 function AboutPage(){
     const isDesktopOrMobile = useMediaQuery({query: '(max-width:768px)'}); // 758px 이하일 때는 모바일 뷰로 바뀐다.
 
@@ -16,7 +21,12 @@ function AboutPage(){
             {isDesktopOrMobile !== true ? 
             <div>
                  <Navbar />
-                <h1>소개 디자인 제작중</h1>
+                 <AboutFirst/>
+                <AboutVision/>
+                <AboutPart/>
+                <AboutProgram/>
+                <AboutMentor/>
+                <AboutLast/>
             </div>
             :
             <div>
