a08e3604a05f6d86e6f0825b17f055f5cd51d5e4
dasdfadssda <quddkflty@gmail.com>
2023-05-17 15:08:54 +0900

    .

--- src/components/Web/Web-HomePage/HomeFirst.js
+++ src/components/Web/Web-HomePage/HomeFirst.js
@@ -2,20 +2,21 @@ import React from 'react';
 import styled from 'styled-components';
 
 const VideoContainer = styled.div`
+  position: fixed;
   top: 0;
   left: 0;
   width: 100%;
-  height: 100vh; /* 변경된 부분 */
+  height: 100vh;
   overflow: hidden;
 `;
 
 const VideoBackground = styled.video`
   width: 100%;
-  height: 100%; /* 변경된 부분 */
+  height: 100%;
   object-fit: cover;
-  position: absolute; /* 변경된 부분 */
-  top: 0; /* 변경된 부분 */
-  left: 0; /* 변경된 부분 */
+  position: absolute;
+  top: 0;
+  left: 0;
   z-index: -999;
 `;
 
