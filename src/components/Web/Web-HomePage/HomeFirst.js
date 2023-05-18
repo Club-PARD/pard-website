c7fe47f4b438f9db13687f77b3d31b7c64b526e8
dnqudgml12 <21800454@handomg.ac.kr>
2023-05-18 11:30:15 +0900

    homefirst수정

--- src/components/Web/Web-HomePage/HomeFirst.js
+++ src/components/Web/Web-HomePage/HomeFirst.js
@@ -1,25 +1,33 @@
-import React from 'react';
+
 import styled from 'styled-components';
+import React, { useEffect, useState } from 'react';
+
 
 const VideoContainer = styled.div`
-  position: relative;
+
+
+ 
   width: 100%;
-  height: 100vh;
+  height: 300vh;
   overflow: hidden;
 `;
 
+
 const VideoBackground = styled.video`
-  width: 100%;
-  height: 100%;
-  object-fit: cover;
-  position: relative;
+width: 100%;
+
+height: 100%;
+object-fit: fill;  // 가로 비율 유지한채 세로를 늘림
   z-index: -999;
+
+
 `;
 
 const HomeFirst = () => {
   return (
     <VideoContainer>
       <VideoBackground autoPlay loop muted>
+       
         <source src={require("../../../assets/Video/BackGroundVideo.mp4")} type="video/mp4" />
       </VideoBackground>
     </VideoContainer>
