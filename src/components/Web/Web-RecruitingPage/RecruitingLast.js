675d3d8f6bcae20e62c3163b838d1e809942a1db
LeeHannaa <tiger5391@naver.com>
2023-05-23 16:57:52 +0900

    RecruitingLastPage

--- src/components/Web/Web-RecruitingPage/RecruitingLast.js
+++ src/components/Web/Web-RecruitingPage/RecruitingLast.js
@@ -3,7 +3,7 @@ import { theme } from '../../../styles/theme';
 import React from 'react';
 
 const PartDiv = styled.div`
-  height: 950px;
+  height: 624px;
   position: relative;
   width: 100%;
   display: flex;
@@ -13,9 +13,9 @@ const PartDiv = styled.div`
   text-align: center;
 `;
 const Div = styled.div`
+  margin: 0px auto;
+  justify-content: center;
   display: flex;
-  flex-direction: column;
-  width: 100%;
   align-items: center;
 `;
 
@@ -24,13 +24,26 @@ const Header7 = styled.div`
   font-weight: ${props => props.theme.fontWeights.Header7};
   color: #FFFFFF;
   font-family: 'NanumSquare Neo';
-font-style: normal;
-font-weight: 800;
-font-size: 40px;
-line-height: 140%;
-margin-top: 550px;
+  font-style: normal;
+  font-weight: 800;
+  font-size: 40px;
+  line-height: 140%;
 `;
-
+const Button = styled.button`
+    font-size: ${props => props.theme.Web_fontSizes.Header6};
+    font-weight: ${props => props.theme.fontWeights.Header6};
+    font-family: 'NanumSquare Neo';
+    display: flex;
+    align-items: center;
+    width: 440px;
+    height: 58px;
+    background-color: #64C59A;
+    justify-content: center;
+    margin-top: 70px;
+    color: #000000;
+    border-radius: 10px;
+    border : none;
+`;
 
 
 function RecruitingLast() {
@@ -38,9 +51,8 @@ function RecruitingLast() {
     <Div>
       <PartDiv>
         <ThemeProvider theme={theme}>
-          <Header7>
-          RecruitingFirst
-          </Header7>
+          <Header7>협업에 미친 파드의 여정에 동참하고 싶다면</Header7>
+          <Button>2기 모집 알림 신청하기</Button>
         </ThemeProvider>
       </PartDiv>
     </Div>
