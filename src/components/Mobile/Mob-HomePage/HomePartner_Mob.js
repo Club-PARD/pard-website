a8fa217abaf27cf44749331bb398402db786e284
dasdfadssda <quddkflty@gmail.com>
2023-07-03 17:04:06 +0900

    Add Partner Logo

--- src/components/Mobile/Mob-HomePage/HomePartner_Mob.js
+++ src/components/Mobile/Mob-HomePage/HomePartner_Mob.js
@@ -1,19 +1,19 @@
-import styled, { ThemeProvider } from 'styled-components';
-import { theme } from '../../../styles/theme';
-import DisquietImage from '../../../assets/img/Disquiet-partner.png';
-import LincImage from '../../../assets/img/Linc-partner.png';
-import ImpactCampusImage from '../../../assets/img/ImpactCampus-partner.png';
-import SImage from '../../../assets/img/S-partner.png';
-import SpackImg from '../../../assets/img/SpackLab.png';
-import AsanImg from '../../../assets/img/ASANImg.png';
-import MobitImg from '../../../assets/img/MobitImg.png';
-
+import styled, { ThemeProvider } from "styled-components";
+import { theme } from "../../../styles/theme";
+import DisquietImage from "../../../assets/img/Disquiet-partner.png";
+import LincImage from "../../../assets/img/Linc-partner.png";
+import ImpactCampusImage from "../../../assets/img/ImpactCampus-partner.png";
+import SImage from "../../../assets/img/S-partner.png";
+import SpackImg from "../../../assets/img/SpackLab.png";
+import AsanImg from "../../../assets/img/ASANImg.png";
+import MobitImg from "../../../assets/img/MobitImg.png";
+import NextChallengeImg from "../../../assets/img/NextChallengeImg.png";
 
 const Header7 = styled.div`
-  font-size: ${props => props.theme.Mob_fontSizes.Header7};
-  font-weight: ${props => props.theme.fontWeights.Header7};
-  color: #FFFFFF;
-  font-family: 'NanumSquare Neo';
+  font-size: ${(props) => props.theme.Mob_fontSizes.Header7};
+  font-weight: ${(props) => props.theme.fontWeights.Header7};
+  color: #ffffff;
+  font-family: "NanumSquare Neo";
   padding-top: 30px;
   white-space: pre-line;
   /* padding-left: 18px; */
@@ -21,132 +21,156 @@ const Header7 = styled.div`
 `;
 
 const Subtitle3 = styled.div`
-    font-size: ${props => props.theme.Mob_fontSizes.Subtitle3};
-    font-weight: ${props => props.theme.fontWeights.Subtitle3};
-    color: #FFFFFF;
-    font-family: 'NanumSquare Neo';
-    white-space: pre-line;
-    padding-top: 30px;
-    /* padding-left: 18px; */
-
+  font-size: ${(props) => props.theme.Mob_fontSizes.Subtitle3};
+  font-weight: ${(props) => props.theme.fontWeights.Subtitle3};
+  color: #ffffff;
+  font-family: "NanumSquare Neo";
+  white-space: pre-line;
+  padding-top: 30px;
+  /* padding-left: 18px; */
 `;
 
 const DisquietImage1 = styled.img`
-    width: 193px;
-    height: 40px;
-    padding-top: 60px;
-    cursor: pointer;
+  width: 193px;
+  height: 40px;
+  padding-top: 60px;
+  cursor: pointer;
 `;
 
 const LincImage2 = styled.img`
-    width: 120px;
-    height: 40px;
-    padding-top: 60px;
-    cursor: pointer;
+  width: 120px;
+  height: 40px;
+  padding-top: 60px;
+  cursor: pointer;
 `;
 
 const ImpactCampusImage3 = styled.img`
-    width: 260px;
-    height: 40px;
-    padding-top: 60px;
-    cursor: pointer;
+  width: 260px;
+  height: 40px;
+  padding-top: 60px;
+  cursor: pointer;
 `;
 
 const SImage4 = styled.img`
-    width: 230px;
-    height: 60px;
-    padding-top: 60px;
-    cursor: pointer;
+  width: 230px;
+  height: 60px;
+  padding-top: 60px;
+  cursor: pointer;
 `;
 
 const Spacklab = styled.img`
-    width: 182px;
-    height: 55px;
-    padding-top: 50px;
-    margin-right: 25px;
-    cursor: pointer;
+  width: 182px;
+  height: 55px;
+  padding-top: 50px;
+  margin-right: 25px;
+  cursor: pointer;
 `;
 
-
 const AsanImgDiv = styled.img`
-    width: 200px;
-    height: 75px;
-    padding-top: 83px;
-    cursor: pointer;
+  width: 200px;
+  height: 75px;
+  padding-top: 83px;
+  cursor: pointer;
+`;
+
+const NextChallenge1Img = styled.img`
+  width: 223px;
+  height: 40px;
+  padding-top: 50px;
+  cursor: pointer;
 `;
 
 const MobitDiv = styled.img`
-    width: 167px;
-    height: 50px;
-    padding-top: 50px;
-    cursor: pointer;
+  width: 167px;
+  height: 50px;
+  padding-top: 50px;
+  cursor: pointer;
 `;
 
 const Div = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
 `;
 const PartDiv = styled.div`
-    padding-top: 131px;
-    height: 950px;
-    padding-left: 10px; 
+  padding-top: 131px;
+  height: 950px;
+  padding-left: 10px;
 `;
 
 const ContentWrapper = styled.div`
-    flex: 1;
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-    align-items: center;
-    margin-top: 43px;
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  margin-top: 43px;
 `;
 
 function HomePartnerMob() {
+  const handleClickSpark = () => {
+    window.open("https://sparklabs.co.kr/lb/index.php", "_blank");
+  };
+  const handleClickDiquiet = () => {
+    window.open("https://disquiet.io/", "_blank");
+  };
+  const handleClickImpactCampus = () => {
+    window.open("https://impact.career/", "_blank");
+  };
+  const handleClickLink = () => {
+    window.open("https://linc30.handong.edu/", "_blank");
+  };
+  const handleClickSwyg = () => {
+    window.open("https://www.swygbro.com", "_blank");
+  };
+  const handleClickAsan = () => {
+    window.open("https://asan-nanum.org", "_blank");
+  };
+  const handleClickMobit = () => {
+    window.open("https://www.momit.co.kr", "_blank");
+  };
+  const handleClickNextChallenge = () => {
+    window.open("https://www.ncf.or.kr/", "_blank");
+  };
 
-    const handleClickSpark = () => {
-        window.open("https://sparklabs.co.kr/lb/index.php", "_blank");
-    };
-    const handleClickDiquiet = () => {
-        window.open("https://disquiet.io/", "_blank");
-    };
-    const handleClickImpactCampus = () => {
-        window.open("https://impact.career/", "_blank");
-    };
-    const handleClickLink = () => {
-        window.open("https://linc30.handong.edu/", "_blank");
-    };
-    const handleClickSwyg = () => {
-        window.open("https://www.swygbro.com", "_blank");
-    };
-    const handleClickAsan = () => {
-        window.open("https://asan-nanum.org", "_blank");
-    };
-    const handleClickMobit = () => {
-        window.open("https://www.momit.co.kr", "_blank");
-    };
-
-    return (
-        <Div>
-            <PartDiv>
-                <ThemeProvider theme={theme}>
-                    <Header7>PARTNERS</Header7>
-                    <Subtitle3>함께하는 성장을 돕는 후원사</Subtitle3>
-                    <ContentWrapper>
-                        <AsanImgDiv onClick={handleClickAsan} src={AsanImg} alt='Asan' />
-                        <Spacklab onClick={handleClickSpark} src={SpackImg} alt='SparkLab' />
-                        <DisquietImage1 onClick={handleClickDiquiet} src={DisquietImage} alt="Disquiet" />
-                        <LincImage2 onClick={handleClickLink} src={LincImage} alt="Linc" />
-                        <ImpactCampusImage3 onClick={handleClickImpactCampus} src={ImpactCampusImage} alt="Impact Campus" />
-                        <SImage4 onClick={handleClickSwyg} src={SImage} alt="S" />
-                        <MobitDiv onClick={handleClickMobit} src={MobitImg} alt="Mobit" />
-                    </ContentWrapper>
-                </ThemeProvider>
-            </PartDiv>
-        </Div>
-    );
+  return (
+    <Div>
+      <PartDiv>
+        <ThemeProvider theme={theme}>
+          <Header7>PARTNERS</Header7>
+          <Subtitle3>함께하는 성장을 돕는 후원사</Subtitle3>
+          <ContentWrapper>
+            <AsanImgDiv onClick={handleClickAsan} src={AsanImg} alt="Asan" />
+            <NextChallenge1Img
+              onClick={handleClickNextChallenge}
+              src={NextChallengeImg}
+              alt="NextChallenge"
+            />
+            <Spacklab
+              onClick={handleClickSpark}
+              src={SpackImg}
+              alt="SparkLab"
+            />
+            <DisquietImage1
+              onClick={handleClickDiquiet}
+              src={DisquietImage}
+              alt="Disquiet"
+            />
+            <LincImage2 onClick={handleClickLink} src={LincImage} alt="Linc" />
+            <ImpactCampusImage3
+              onClick={handleClickImpactCampus}
+              src={ImpactCampusImage}
+              alt="Impact Campus"
+            />
+            <SImage4 onClick={handleClickSwyg} src={SImage} alt="S" />
+            <MobitDiv onClick={handleClickMobit} src={MobitImg} alt="Mobit" />
+          </ContentWrapper>
+        </ThemeProvider>
+      </PartDiv>
+    </Div>
+  );
 }
 
 export default HomePartnerMob;
--- src/components/Web/Web-HomePage/HomePartner.js
+++ src/components/Web/Web-HomePage/HomePartner.js
@@ -1,150 +1,171 @@
-import styled, { ThemeProvider } from 'styled-components';
-import { theme } from '../../../styles/theme';
-import DisquietImage from '../../../assets/img/Disquiet-partner.png';
-import LincImage from '../../../assets/img/Linc-partner.png';
-import ImpactCampusImage from '../../../assets/img/ImpactCampus-partner.png';
-import SImage from '../../../assets/img/S-partner.png';
-import SpackImg from '../../../assets/img/SpackLab.png';
-import AsanImg from '../../../assets/img/ASANImg.png';
-import MobitImg from '../../../assets/img/MobitImg.png';
-
+import styled, { ThemeProvider } from "styled-components";
+import { theme } from "../../../styles/theme";
+import DisquietImage from "../../../assets/img/Disquiet-partner.png";
+import LincImage from "../../../assets/img/Linc-partner.png";
+import ImpactCampusImage from "../../../assets/img/ImpactCampus-partner.png";
+import SImage from "../../../assets/img/S-partner.png";
+import SpackImg from "../../../assets/img/SpackLab.png";
+import AsanImg from "../../../assets/img/ASANImg.png";
+import MobitImg from "../../../assets/img/MobitImg.png";
+import NextChallengeImg from "../../../assets/img/NextChallengeImg.png";
 
 const Header2 = styled.div`
-  font-size: ${props => props.theme.Web_fontSizes.Header2};
-  font-weight: ${props => props.theme.fontWeights.Header2};
-  color: #FFFFFF;
-  font-family: 'NanumSquare Neo';
+  font-size: ${(props) => props.theme.Web_fontSizes.Header2};
+  font-weight: ${(props) => props.theme.fontWeights.Header2};
+  color: #ffffff;
+  font-family: "NanumSquare Neo";
   margin-bottom: 25px;
   white-space: pre-line;
-  padding-left:125px;
+  padding-left: 125px;
 `;
 
 const Header4 = styled.div`
-    font-size: ${props => props.theme.Web_fontSizes.Header4};
-    font-weight: ${props => props.theme.fontWeights.Header4};
-    color: #FFFFFF;
-    font-family: 'NanumSquare Neo';
-    white-space: pre-line;
-    padding-left:125px;
-    padding-right: 80px;
+  font-size: ${(props) => props.theme.Web_fontSizes.Header4};
+  font-weight: ${(props) => props.theme.fontWeights.Header4};
+  color: #ffffff;
+  font-family: "NanumSquare Neo";
+  white-space: pre-line;
+  padding-left: 125px;
+  padding-right: 80px;
 `;
 
 const PartDiv = styled.div`
-      margin: 0px auto;
-    height: 700px;
-    justify-content: center;
-    width: 1330px;
+  margin: 0px auto;
+  height: 700px;
+  justify-content: center;
+  width: 1330px;
 `;
 
 const DisquietImage1 = styled.img`
-    width: 241px;
-    height: 50px;
-    padding-right: 65px;
-    cursor: pointer;
+  width: 241px;
+  height: 50px;
+  padding-right: 65px;
+  cursor: pointer;
 `;
 
 const LincImage2 = styled.img`
-    width: 150px;
-    height: 50px;
-    padding-left: 90px;
-    cursor: pointer;
+  width: 150px;
+  height: 50px;
+  padding-left: 90px;
+  cursor: pointer;
 `;
 
 const ImpactCampusImage3 = styled.img`
-    width: 366px;
-    height: 50px;
-    cursor: pointer;
+  width: 366px;
+  height: 50px;
+  cursor: pointer;
 `;
 
 const SImage4 = styled.img`
-    width: 240px;
-    height: 70px;
-    cursor: pointer;
+  width: 240px;
+  height: 70px;
+  cursor: pointer;
+`;
+
+const NextChallenge1Img = styled.img`
+  width: 223px;
+  height: 40px;
+  margin-right: 60px;
+  cursor: pointer;
 `;
 
 const SpackImgDiv = styled.img`
-    width: 217px;
-    height: 60px;
-    margin-right: 65px;
-    cursor: pointer;
+  width: 217px;
+  height: 60px;
+  margin-right: 65px;
+  cursor: pointer;
 `;
 
 const AsanImgDiv = styled.img`
-    width: 223px;
-    height: 90px;
-    margin-right: 65px;
-    cursor: pointer;
+  width: 223px;
+  height: 90px;
+  margin-right: 65px;
+  cursor: pointer;
 `;
 
 const MobitDiv = styled.img`
-    width: 167px;
-    height: 50px;
-    margin-left: 65px;
-    cursor: pointer;
+  width: 167px;
+  height: 50px;
+  margin-left: 65px;
+  cursor: pointer;
 `;
 
 const ContentWrapper = styled.div`
-    flex: 1;
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    margin-top: 120px;
-    justify-content: center;
+  flex: 1;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  margin-top: 120px;
+  justify-content: center;
 `;
 
 const ContentWrapper2 = styled.div`
-    flex: 1;
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    margin-top: 50px;
-    justify-content: center;
+  flex: 1;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  margin-top: 50px;
+  justify-content: center;
 `;
 
 function HomePartner() {
+  const handleClickSpark = () => {
+    window.open("https://sparklabs.co.kr/lb/index.php", "_blank");
+  };
+  const handleClickDiquiet = () => {
+    window.open("https://disquiet.io/", "_blank");
+  };
+  const handleClickImpactCampus = () => {
+    window.open("https://impact.career/", "_blank");
+  };
+  const handleClickLink = () => {
+    window.open("https://linc30.handong.edu/", "_blank");
+  };
+  const handleClickSwyg = () => {
+    window.open("https://www.swygbro.com", "_blank");
+  };
+  const handleClickAsan = () => {
+    window.open("https://asan-nanum.org/", "_blank");
+  };
+  const handleClickMobit = () => {
+    window.open("https://www.momit.co.kr", "_blank");
+  };
+  const handleClickNextChallenge = () => {
+    window.open("https://www.ncf.or.kr/", "_blank");
+  };
 
-
-    const handleClickSpark = () => {
-        window.open("https://sparklabs.co.kr/lb/index.php", "_blank");
-    };
-    const handleClickDiquiet = () => {
-        window.open("https://disquiet.io/", "_blank");
-    };
-    const handleClickImpactCampus = () => {
-        window.open("https://impact.career/", "_blank");
-    };
-    const handleClickLink = () => {
-        window.open("https://linc30.handong.edu/", "_blank");
-    };
-    const handleClickSwyg = () => {
-        window.open("https://www.swygbro.com", "_blank");
-    };
-    const handleClickAsan = () => {
-        window.open("https://asan-nanum.org/", "_blank");
-    };
-    const handleClickMobit = () => {
-        window.open("https://www.momit.co.kr", "_blank");
-    };
-
-    return (
-        <PartDiv>
-            <ThemeProvider theme={theme}>
-                <Header2>PARTNERS</Header2>
-                <Header4>함께하는 성장을 돕는 후원사</Header4>
-                <ContentWrapper>
-                    <AsanImgDiv onClick={handleClickAsan} src={AsanImg} ast="Asan" />
-                    <SpackImgDiv onClick={handleClickSpark} src={SpackImg} alt="S" />
-                    <ImpactCampusImage3 onClick={handleClickImpactCampus} src={ImpactCampusImage} alt="Impact Campus" />
-                </ContentWrapper>
-                <ContentWrapper2>
-                    <DisquietImage1 onClick={handleClickDiquiet} src={DisquietImage} alt="Disquiet" />
-                    <SImage4 onClick={handleClickSwyg} src={SImage} alt="S" />
-                    <LincImage2 onClick={handleClickLink} src={LincImage} alt="Linc" />
-                    <MobitDiv onClick={handleClickMobit} src={MobitImg} alt="Mobit" />
-                </ContentWrapper2>
-            </ThemeProvider>
-        </PartDiv>
-    );
+  return (
+    <PartDiv>
+      <ThemeProvider theme={theme}>
+        <Header2>PARTNERS</Header2>
+        <Header4>함께하는 성장을 돕는 후원사</Header4>
+        <ContentWrapper>
+          <AsanImgDiv onClick={handleClickAsan} src={AsanImg} ast="Asan" />
+          <NextChallenge1Img
+            onClick={handleClickNextChallenge}
+            src={NextChallengeImg}
+            alt="NextChallenge"
+          />
+          <SpackImgDiv onClick={handleClickSpark} src={SpackImg} alt="S" />
+          <ImpactCampusImage3
+            onClick={handleClickImpactCampus}
+            src={ImpactCampusImage}
+            alt="Impact Campus"
+          />
+        </ContentWrapper>
+        <ContentWrapper2>
+          <DisquietImage1
+            onClick={handleClickDiquiet}
+            src={DisquietImage}
+            alt="Disquiet"
+          />
+          <SImage4 onClick={handleClickSwyg} src={SImage} alt="S" />
+          <LincImage2 onClick={handleClickLink} src={LincImage} alt="Linc" />
+          <MobitDiv onClick={handleClickMobit} src={MobitImg} alt="Mobit" />
+        </ContentWrapper2>
+      </ThemeProvider>
+    </PartDiv>
+  );
 }
 
 export default HomePartner;
