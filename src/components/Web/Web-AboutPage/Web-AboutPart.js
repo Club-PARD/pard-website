ad7105e7edb0bbbe7678d7721fedc6f8c6ab8427
codms0201 <chanyeol0615@naver.com>
2024-08-13 13:20:36 +0900

    fix: intro page content

--- src/components/Web/Web-AboutPage/Web-AboutPart.js
+++ src/components/Web/Web-AboutPage/Web-AboutPart.js
@@ -1,9 +1,9 @@
 import styled, { ThemeProvider } from "styled-components";
 import { theme } from "../../../styles/theme";
 import React, { useState } from "react";
-import PM from "../../../assets/img/PM_info.png";
-import Web from "../../../assets/img/Web_info.png";
-import Design from "../../../assets/img/Design_info.png";
+import PM from "../../../assets/img/PM_Info.png";
+import Web from "../../../assets/img/WEB_Info.png";
+import Design from "../../../assets/img/Design_Info.png";
 import App from "../../../assets/img/iOS_info.png";
 import Server from "../../../assets/img/Server_info.png";
 
@@ -13,28 +13,25 @@ function AboutPart() {
       id: 1,
       part: "서비스 기획",
       border_color: "#5262F5",
-      imgsrc: PM,
+      imgsrc: PM, //수정 필요
       selected: true,
       partEng: "Product Manager",
       context: (
         <>
-          사용자의 문제를 찾고, 해결할 수 있는 서비스를 기획합니다.
+          뾰족한 문제정의를 바탕으로 사용자들의 문제를 해결하는 서비스를 기획합니다.
           <br />
           아이디어 발상에서 그치는 것이 아닌 프로덕트 구현의 모든 과정에서
           <br />
           관리자 역할을 수행합니다. 이 과정에서 실무 기획에서 사용되는
           <br />
-          IA, Flow-Chart, 화면 설계서 등의 기획 산출물을 작성하고 산출물
-          기반으로
-          <br />
-          디자이너/개발자와 소통합니다.
+          기획 산출물을 작성하고 산출물을 기반으로 디자이너/개발자와소통합니다.
           <br />
           <br />
-          서비스 기획에 필요한 문제 정의, 데이터 기반 솔루션 도출, UX 설계 뿐만
-          아니라
+          이 모든 과정을 통해 문제 정의, 데이터 기반 솔루션 도출,
           <br />
-          프로젝트 매니징을 위한 프레임워크, 회고 방법 등에 기반해 <br />
-          서비스 기획자 및 PM의 마인드를 가질 수 있습니다.
+          UX 설계 뿐만 아니라 프로젝트 매니징을 위한 프레임워크,
+          <br />
+          회고 방법을 통해 서비스 기획자 및 PM의 역량을 쌓을 수 있습니다.
         </>
       ),
       homework: "서비스 화면 설계서 작성",
@@ -43,27 +40,23 @@ function AboutPart() {
       id: 2,
       part: "UI/UX 디자인",
       border_color: "#7B3FEF",
-      imgsrc: Design,
+      imgsrc: Design, //수정 필요
       selected: false,
       partEng: "Designer",
       context: (
         <>
-          UX/UI 디자인 이론을 배우며, 사용자 중심 사고를 기르고
+          사용자 심리에 기반한 UX/UI 디자인 이론을 학습하고,
           <br />
-          개발 가능한 디자인을 구현합니다.
+          디자인 구현의 모든 단계에서 사용자 중심 사고를 적용하여
           <br />
-          각자 하나의 App/Web Product를 선택해 배운 내용들을
-          <br />
-          적용해보고 다각도에서 분석하면서 다양한 역량을 키우고 동시에
-          <br />
-          유의미한 포트폴리오를 구축합니다.
+          실질적인 문제 해결 능력을 학습합니다.
           <br />
           <br />
-          현업 실무자 분들, 그리고 타 파트와의 협업을 통해 디자인 뿐 아니라
+          현업 실무자와의 멘토링, 그리고 다른 직군과의 프로젝트 경험을 통해,
           <br />
-          기획, 사용자 조사, 데이터 도출, 개발자와 협업 등 비즈니스에 대한
+          서비스 기획, 사용자 리서치, 데이터 분석, QA 등 실무에서 활용가능한
           <br />
-          이해와 전문성을 갖추어 협업에 최적화된 디자이너로 성장합니다.
+          전문성을 기르고 궁극적으로 협업에 강한 디자이너로 성장합니다.
         </>
       ),
       homework: "서비스 리디자인 프로젝트",
@@ -77,19 +70,22 @@ function AboutPart() {
       partEng: "iOS Developer",
       context: (
         <>
-          iOS 앱과 서비스 개발을 위한 이론을 배우고 실습/협업 합니다. <br />
-          개인이 아닌 팀으로 이루어진 개발 과제와 프로젝트를 통해
+          iOS 앱과 서비스 개발을 위한 이론을 배우고 실습/협업합니다.
           <br />
-          Github의 활용과 응용은 물론 프로젝트 파일 생성부터 배포까지의 과정을
+          애플 자체 개발 언어인 Swift를 통해 UI를 효율적으로 구현하고
           <br />
-          경험할 수 있습니다.
+          이를 바탕으로 팀 프로젝트를 진행함으로써 실전 경험을 쌓으며
+          <br />
+          팀원들과 함께 성장합니다.
           <br />
           <br />
-          프레임 워크에 대한 깊은 분석과 역량을 키우고, <br />
-          IT 프로덕트 제작 과정을 경험함으로써 유의미한
+          최신 iOS 기술 트렌드를 공부하고, 이를 실무에 적용하여
           <br />
-          포트폴리오를 가진 개발자로 성장합니다.
-          <br />* 1기는 Flutter를, 2기부터는 iOS 기반으로 학습합니다.
+          사용자 친화적인 앱을 개발하는 것을 목표로 하며 프레임워크에 대한
+          <br />
+          분석 능력과 역량을 키우고, IT 프로덕트 제작 과정을 경험함으로써
+          <br />
+          유의미한 포트폴리오를 가진 개발자로 성장합니다.
         </>
       ),
       homework: "netflix 앱 클론 코딩",
@@ -98,30 +94,25 @@ function AboutPart() {
       id: 4,
       part: "웹(WEB)",
       border_color: "#FF5C00",
-      imgsrc: Web,
+      imgsrc: Web, //수정 필요
       selected: false,
       partEng: "Web Developer",
       context: (
         <>
           <br />
-          웹과 서비스 개발을 위한 이론을 배우고 실습/협업 합니다.
-          <br />
-          HTML/CSS/JS에 대해 기초를 빠르게 다지고
-          <br />
-          React 라이브러리를 기반으로 웹 서비스를 개발할 뿐 아니라
-          <br />팀 프로젝트를 통해 이론으로 습득할 수 없는 실제적 경험과 역량을
-          <br />
-          극대화합니다.
-          <br />
-          <br />
-          기획, 디자인 직군과의 실제 배포를 위한 제대로 된 협업을 통해서
+          기획, 디자인 직군과의 실제 배포를 위한 제대로된 협업을 통해서
           <br />
           인공지능이 대체할 수 없는 커뮤니케이션 능력을 갖추고,
           <br />
           기획자와 디자이너를 이해할 수 있는 개발자로 성장합니다.
+          <br />
+          <br />
+          또한, 웹 개발자간의 협업, 웹과 서버 개발자간의 협업을 통해서
+          <br />
+          유사 직군과 함께 하는 과정을 경험하며 성장합니다.
         </>
       ),
-      homework: "Instagram 웹 클론 코딩",
+      homework: "오늘의 집 웹 클론 코딩",
     },
     {
       id: 5,
@@ -133,17 +124,18 @@ function AboutPart() {
       context: (
         <>
           <br />
-          실제 서비스를 운영하기 위해 데이터 설계부터 서버를 관리하고,
+          데이터 설계, 서버 관리, 그리고 시스템 인프라 구축 및 운영에 대한
           <br />
-          전반적인 시스템 인프라를 구축 및 배포 및 운영합니다. 자바의
+          깊이 있는이해를 바탕으로 실제 서비스 운영에 기여합니다.
           <br />
-          객체지향, 서버, 관계형 데이터베이스를 이해하고, Spring Boot의
+          자바의 객체지향 프로그래밍 원칙과 관계형 데이터베이스 구조를 이해하며,
           <br />
-          기본기를 다지며 다양한 API를 구현하는 개발자로 성장합니다.
-          <br /> <br />
-          클라이언트를 담당하는 앱/웹 파트와 협업하며 의사소통 능력을
+          Spring Boot를 활용하여 기본기부터 보안까지 폭넓은 경험을 쌓고 있습니다.
           <br />
-          키우고, CI/CD를 구축할 수 있는 개발자로 성장합니다.
+          <br />
+          클라이언트를 담당하는 앱/웹 파트와 협업하며 의사소통 능력을 키우고,
+          <br />
+          CI/CD를 구축할 수 있는 개발자로 성장합니다.
         </>
       ),
       homework: "ERD(Entity Relationship Diagram)",
