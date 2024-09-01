33a570dc49ee6401a4030cd8e9a05f4f53904e42
codms0201 <chanyeol0615@naver.com>
2024-09-01 15:01:26 +0900

    fix: recruting state

--- src/utils/data.constant.js
+++ src/utils/data.constant.js
@@ -8,6 +8,6 @@ export const pardDATA = {
 // 사전 알림 구글 폼 URL
 export const EARLY_FORM_URL = "https://open.kakao.com/o/gHDyOT5f";
 // 신청 구글 폼 URL
-export const APPLY_FORM_URL = "https://we-pard.notion.site/PARD-4-8c664762477842589579340a73752ca0?pvs=74";
+export const APPLY_FORM_URL = "https://we-pard.notion.site/PARD-4-2-9acffd6ed78e4605b83714a298dbe907?pvs=4";
 
 export const PROJECT_GRID = 9;
